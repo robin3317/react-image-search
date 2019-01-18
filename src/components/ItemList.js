64ae2762a14055a40eedeebc8ef0b49a1aaace63
robin3317 <abdurrahmanrobin47@gmail.com>
2019-01-19 05:39:16 +0600

    testing 2

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const ImageList = props => {
   const images = props.images.map(image => {
-    return <img src={image.urls.regular} alt={image.description} />
+    return <img key={image.id} src={image.urls.regular} alt={image.description} />
   })
 
-  return <div>{images}</div>;
+  return <div>{images}</div>
 }
 
 export default ImageList
